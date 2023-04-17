832d438f1918b2b32c260671726db5948b043a0c
vyomkeshj <vyomkeshjha@yahoo.com>
2023-04-17 10:56:25 +0200

    Added to text

--- src/components/Intro4/intro4.jsx
+++ src/components/Intro4/intro4.jsx
@@ -33,7 +33,7 @@ const Intro4 = ({ sliderRef, blackStar }) => {
                           <span className="color-font">Software<sup>*</sup>&nbsp;</span>defined<br/><span className="color-font">
                           <Typewriter
                               options={{
-                                  strings: ['data', 'analytics', 'simulation', 'api', 'web-apps', 'magic'],
+                                  strings: ['ML Flows', 'Analytics', 'Web-Apps', 'Simulation', 'Prototyping', 'Automation'],
                                   autoStart: true,
                                   delay: 20,
                                   loop: true,
